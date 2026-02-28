0ac89d2de4b13bc902b86e10c45a401969bb5288
Daniel <dmelchor@gmail.com>
2026-02-27 20:57:11 -0600

    ver ruta de imagenes en informe

--- app/Http/Controllers/AdmisionController.php
+++ app/Http/Controllers/AdmisionController.php
@@ -1645,6 +1645,7 @@ class AdmisionController extends Controller
         $firma = Medico::findOrFail($medico->medico_id)->select(DB::raw('CONCAT(titulo, " ", nombre_completo) as nombre_profesional'),'firma')->first();
 
         $fotos = AdmisionAtencionImagen::where('admision_atencion_id', $atencion_id)->get();
+        dd($fotos);
 
         // return view('admisiones.informe', compact('pEmpresa', 'dia', 'nombre_mes', 'anio', 'registro', 'fotos'));
 
