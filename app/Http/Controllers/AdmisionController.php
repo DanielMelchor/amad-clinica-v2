0754567a8535843dc9e9fc72797e4bb617683231
Daniel <dmelchor@gmail.com>
2026-02-27 21:02:45 -0600

    ver ruta de imagenes en informe

--- app/Http/Controllers/AdmisionController.php
+++ app/Http/Controllers/AdmisionController.php
@@ -1646,12 +1646,12 @@ class AdmisionController extends Controller
 
         $fotos = AdmisionAtencionImagen::where('admision_atencion_id', $atencion_id)->get();
 
-        return view('admisiones.informe', compact('pEmpresa', 'dia', 'nombre_mes', 'anio', 'registro', 'fotos'));
+        //return view('admisiones.informe', compact('pEmpresa', 'dia', 'nombre_mes', 'anio', 'registro', 'fotos'));
 
-        /*$pdf = Pdf::loadView('admisiones.informe', compact('pEmpresa', 'dia', 'nombre_mes', 'anio', 'registro', 'fotos', 'firma', 'rutaLogo'));
+        $pdf = Pdf::loadView('admisiones.informe', compact('pEmpresa', 'dia', 'nombre_mes', 'anio', 'registro', 'fotos', 'firma', 'rutaLogo'));
         $pdf->setPaper([0, 0, 612, 396], 'landscape');
 
-        return $pdf->stream('informe_' . $registro->paciente_nombre . '.pdf');*/
+        return $pdf->stream('informe_' . $registro->paciente_nombre . '.pdf');
         
     }
 
